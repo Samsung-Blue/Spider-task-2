9c5e504b9ea175e9c1e73fb29f86351cdae09646
Samsung-Blue <sreelakshmias97@gmail.com>
2016-06-16 20:28:03 +0530

    Add edit button

--- Students/server.js
+++ Students/server.js
@@ -1,8 +1,11 @@
+/* global newform */
+
 var mysql = require('mysql');
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var app = express();
+var pass;
 app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : '',
+  password : 'infinity',
   port : '8801',
   database : 'student'
 });
@@ -21,7 +24,9 @@ connection.connect(function(err){
     else
         console.log("Database connected");
 });
-connection.query('CREATE TABLE students(Rollno int UNIQUE,Name varchar(50),Dept varchar(5),Email varchar(100),Address varchar(100),About varchar(100),CONSTRAINT chk CHECK(dept=CSE OR dept=ECE OR dept=CIVIL OR dept=CHEM OR dept=EEE OR dept=ICE OR dept=PROD OR dept=META OR dept=MECH))',function(err,result){
+connection.query('DROP TABLE students',function(err,result){
+    });
+connection.query('CREATE TABLE students(Rollno int UNIQUE,Name varchar(50),Dept varchar(5),Email varchar(100),Address varchar(100),About varchar(100),passcode varchar(5) UNIQUE)',function(err,result){
     if(err)
         throw err;
     else
@@ -31,16 +36,17 @@ app.get('/',function(req,res){
    displayForm(res);
 });
 app.post('/',function(req,res){
-    if(req.body.submit==='view')
+    if(req.body.submit==='Search')
     {
-    connection.query('SELECT * FROM students',function(err,rows,fields){
+        var roll = req.body.rollno;
+    connection.query('SELECT * FROM students WHERE Rollno = ?',roll,function(err,rows,fields){
        if(!err)
-         res.send(rows);
+          res.send(rows);
        else
            console.log("Error has occurred");
     });
     }
-    else if(req.body.submit==='add')
+    else if(req.body.submit==='Add')
     {
         var post={
             rollno : req.body.roll,
@@ -48,8 +54,10 @@ app.post('/',function(req,res){
             dept : req.body.dept,
             email : req.body.email,
             address : req.body.address,
-            about : req.body.about
+            about : req.body.about,
+            passcode : Math.floor(900*Math.random())
         };
+        
         connection.query('INSERT INTO students SET ?',post,function(err,result){
             if(!err)
             {
@@ -61,7 +69,23 @@ app.post('/',function(req,res){
                 
         });
     }
+    else if(req.body.submit==='Edit')
+    {
+        console.log("Edit!!!");
+        pass=req.body.passcode;
+        connection.query('UPDATE students SET Rollno=?, Name=?, Dept = ?,Email=?,Address=?,About=? WHERE passcode = ?',[req.body.rollnew,req.body.namenew,req.body.deptnew,req.body.emailnew,req.body.addressnew,req.body.aboutnew,pass],function(err,result){
+        if(!err)
+        {
+            console.log("Updated");
+            console.log(req.body.rollnew);
+        }
+        else
+            console.log("Error has occured");
+    });
+    displayForm(res);
+    }
 });
+
 function displayForm(res)
 {
     fs.readFile('form.html',function(err,data){
